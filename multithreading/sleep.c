8b5b1224baa5b92291f7395772340036bb32c9de
cjrevelas <cjrevelas@gmail.com>
2025-11-08 17:45:44 +0200

    fix compilation error and warning

--- multithreading/sleep.c
+++ multithreading/sleep.c
@@ -2,7 +2,7 @@
 #include <unistd.h>
 #include <pthread.h>
 
-void *function()
+void *print(void *arg)
 {
 	printf("Thread started executing the function\n");
 
@@ -10,6 +10,8 @@ void *function()
 	sleep(3);
 
 	printf("Thread finished executing the function\n");
+
+  return (void *)NULL;
 }
 
 int main()
@@ -17,8 +19,8 @@ int main()
 	pthread_t t1, t2;
 
 	// Create the new threads and assign them the "function" task
-	pthread_create(&t1, NULL, &function, NULL);
-	pthread_create(&t2, NULL, &function, NULL);
+	pthread_create(&t1, NULL, &print, NULL);
+	pthread_create(&t2, NULL, &print, NULL);
 
 	// Wait for the threads to end their task
 	pthread_join(t1, NULL);
